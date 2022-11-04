142aa43f2e59c6d6213eade644a9cce37521f28d
Benoy Eremita <benoy@segmenty.com>
2022-11-04 16:46:42 +0530

    update the database methods file

--- DataTypeValidation_Insertion_Training/DataTypeValidation.py
+++ DataTypeValidation_Insertion_Training/DataTypeValidation.py
@@ -24,16 +24,10 @@ class DBOperation:
         Create database --> open the connection.
         returns: None
         """
-        try:
-            conn = sqlite3.connect(self.path + database_name + '.db')
-            file = open("Training_Logs/DataBaseConnectionLog.txt", 'a+')
-            self.logger.log(file, "Opened %s database successfully" % database_name)
-            file.close()
-        except ConnectionError:
-            file = open("Training_Logs/DataBaseConnectionLog.txt", 'a+')
-            self.logger.log(file, "Error while connecting to database: %s" % ConnectionError)
-            file.close()
-            raise ConnectionError
+        conn = sqlite3.connect(self.path + database_name + '.db')
+        file = open("Training_Logs/DataBaseConnectionLog.txt", 'a+')
+        self.logger.log(file, "Opened %s database successfully" % database_name)
+        file.close()
         return conn
 
     def create_table_in_db(self, database_name, column_names):
@@ -43,153 +37,96 @@ class DBOperation:
         creates a database connection -->
         returns: None
         """
-        try:
-            conn = self.create_data_base_connection(database_name)
-            c = conn.cursor()
-            c.execute("SELECT count(name)  FROM sqlite_master WHERE type = 'table' AND name = 'Good_Raw_Data'")
-            if c.fetchone()[0] == 1:
-                conn.close()
-                file = open("Training_Logs/DbTableCreateLog.txt", 'a+')
-                self.logger.log(file, "Tables created successfully!!")
-                file.close()
-
-                file = open("Training_Logs/DataBaseConnectionLog.txt", 'a+')
-                self.logger.log(file, "Closed %s database successfully" % database_name)
-                file.close()
-
-            else:
-
-                for key in column_names.keys():
-                    type = column_names[key]
-                    # in try block we check if the table exists, if yes then add columns to the table
-                    # else in catch block we will create the table
-                    try:
-                        # cur = cur.execute("SELECT name FROM {dbName} WHERE type='table' AND name='Good_Raw_Data'".format(dbName=database_name))
-                        conn.execute(
-                            'ALTER TABLE Good_Raw_Data ADD COLUMN "{column_name}" {dataType}'.format(column_name=key,
-                                                                                                     dataType=type))
-                    except:
-                        conn.execute('CREATE TABLE  Good_Raw_Data ({column_name} {dataType})'.format(column_name=key,
-                                                                                                     dataType=type))
-
-                    # try:
-                    #     #cur.execute("SELECT name FROM {dbName} WHERE type='table' AND name='Bad_Raw_Data'".format(dbName=database_name))
-                    #     conn.execute('ALTER TABLE Bad_Raw_Data ADD COLUMN "{column_name}" {dataType}'.format(column_name=key,dataType=type))
-                    #
-                    # except:
-                    #     conn.execute('CREATE TABLE Bad_Raw_Data ({column_name} {dataType})'.format(column_name=key, dataType=type))
-
-                conn.close()
-
-                file = open("Training_Logs/DbTableCreateLog.txt", 'a+')
-                self.logger.log(file, "Tables created successfully!!")
-                file.close()
-
-                file = open("Training_Logs/DataBaseConnectionLog.txt", 'a+')
-                self.logger.log(file, "Closed %s database successfully" % database_name)
-                file.close()
-
-        except Exception as e:
-            file = open("Training_Logs/DbTableCreateLog.txt", 'a+')
-            self.logger.log(file, "Error while creating table: %s " % e)
-            file.close()
+        conn = self.create_data_base_connection(database_name)
+        c = conn.cursor()
+        c.execute("SELECT count(name)  FROM sqlite_master WHERE type = 'table' AND name = 'Good_Raw_Data'")
+        if c.fetchone()[0] == 1:
             conn.close()
+            file = open("Training_Logs/DbTableCreateLog.txt", 'a+')
+            self.logger.log(file, "Tables created successfully!!")
+            file.close()
             file = open("Training_Logs/DataBaseConnectionLog.txt", 'a+')
             self.logger.log(file, "Closed %s database successfully" % database_name)
             file.close()
-            raise e
+        else:
+            for key in column_names.keys():
+                _type = column_names[key]
+                # in try block we check if the table exists, if yes then add columns to the table
+                # else in the except block we will create the table
+                try:
+                    conn.execute(
+                        'ALTER TABLE Good_Raw_Data ADD COLUMN "{column_name}" {dataType}'.format(column_name=key,
+                                                                                                 dataType=_type))
+                except:
+                    conn.execute('CREATE TABLE  Good_Raw_Data ({column_name} {dataType})'.format(column_name=key,
+                                                                                                 dataType=_type))
+            conn.close()
+            file = open("Training_Logs/DbTableCreateLog.txt", 'a+')
+            self.logger.log(file, "Tables created successfully!!")
+            file.close()
+            file = open("Training_Logs/DataBaseConnectionLog.txt", 'a+')
+            self.logger.log(file, "Closed %s database successfully" % database_name)
+            file.close()
 
-    def insertIntoTableGoodData(self, Database):
-
+    def insert_good_data_into_table(self, database):
         """
-                               Method Name: insertIntoTableGoodData
-                               Description: This method inserts the Good data files from the Good_Raw folder into the
-                                            above created table.
-                               Output: None
-                               On Failure: Raise Exception
-
-                                Written By: iNeuron Intelligence
-                               Version: 1.0
-                               Revisions: None
-
+        This method inserts the Good data files from the Good_Raw folder into the above created table in the database.
+        params: database
+        Iterate over all files in the good data directory --> open each file --> get the values in each line -->
+        write to the database table --> close the connection.
+        returns: None
         """
-
-        conn = self.create_data_base_connection(Database)
-        goodFilePath = self.goodFilePath
-        badFilePath = self.badFilePath
-        onlyfiles = [f for f in listdir(goodFilePath)]
+        conn = self.create_data_base_connection(database)
+        good_file_path = self.goodFilePath
+        bad_file_path = self.badFilePath
+        all_files = [f for f in listdir(good_file_path)]
         log_file = open("Training_Logs/DbInsertLog.txt", 'a+')
-
-        for file in onlyfiles:
+        for file in all_files:
             try:
-                with open(goodFilePath + '/' + file, "r") as f:
+                with open(good_file_path + '/' + file, "r") as f:
                     next(f)
                     reader = csv.reader(f, delimiter="\n")
-                    for line in enumerate(reader):
-                        for list_ in (line[1]):
-                            try:
-                                conn.execute('INSERT INTO Good_Raw_Data values ({values})'.format(values=(list_)))
-                                self.logger.log(log_file, " %s: File loaded successfully!!" % file)
-                                conn.commit()
-                            except Exception as e:
-                                raise e
-
+                    for idx, line in enumerate(reader):
+                        for values in line:
+                            conn.execute('INSERT INTO Good_Raw_Data values ({values})'.format(values=values))
+                            self.logger.log(log_file, " %s: File loaded successfully!!" % file)
+                            conn.commit()
             except Exception as e:
-
                 conn.rollback()
                 self.logger.log(log_file, "Error while creating table: %s " % e)
-                shutil.move(goodFilePath + '/' + file, badFilePath)
+                shutil.move(good_file_path + '/' + file, bad_file_path)
                 self.logger.log(log_file, "File Moved Successfully %s" % file)
                 log_file.close()
                 conn.close()
-
         conn.close()
         log_file.close()
 
-    def selectingDatafromtableintocsv(self, Database):
-
+    def export_good_data_to_csv(self, database):
         """
-                               Method Name: selectingDatafromtableintocsv
-                               Description: This method exports the data in GoodData table as a CSV file. in a given location.
-                                            above created .
-                               Output: None
-                               On Failure: Raise Exception
-
-                                Written By: iNeuron Intelligence
-                               Version: 1.0
-                               Revisions: None
-
+        This method exports the data in the good_data table to a csv file.
+        params: database
+        Select the data from Good_Raw_Data directory --> get the headers of the file --> create the csv file
+        --> write headers to csv --> write the data to the csv file.
+        returns: None
         """
-
-        self.fileFromDb = 'Training_FileFromDB/'
-        self.fileName = 'InputFile.csv'
+        file_from_db = 'Training_FileFromDB/'
+        file_name = 'InputFile.csv'
         log_file = open("Training_Logs/ExportToCsv.txt", 'a+')
-        try:
-            conn = self.create_data_base_connection(Database)
-            sqlSelect = "SELECT *  FROM Good_Raw_Data"
-            cursor = conn.cursor()
-
-            cursor.execute(sqlSelect)
-
-            results = cursor.fetchall()
-            # Get the headers of the csv file
-            headers = [i[0] for i in cursor.description]
-
-            # Make the CSV ouput directory
-            if not os.path.isdir(self.fileFromDb):
-                os.makedirs(self.fileFromDb)
-
-            # Open CSV file for writing.
-            csvFile = csv.writer(open(self.fileFromDb + self.fileName, 'w', newline=''), delimiter=',',
-                                 lineterminator='\r\n', quoting=csv.QUOTE_ALL, escapechar='\\')
-
-            # Add the headers and data to the CSV file.
-            csvFile.writerow(headers)
-            csvFile.writerows(results)
-
-            self.logger.log(log_file, "File exported successfully!!!")
-            log_file.close()
-
-        except Exception as e:
-            self.logger.log(log_file, "File exporting failed. Error : %s" % e)
-            log_file.close()
+        conn = self.create_data_base_connection(database)
+        sql_select = "SELECT *  FROM Good_Raw_Data"
+        cursor = conn.cursor()
+        cursor.execute(sql_select)
+        results = cursor.fetchall()
+        # Get the headers of the csv file
+        headers = [i[0] for i in cursor.description]
+        # Make the CSV output directory
+        if not os.path.isdir(file_from_db):
+            os.makedirs(file_from_db)
+        # Open CSV file for writing.
+        csv_file = csv.writer(open(file_from_db + file_name, 'w', newline=''), delimiter=',',
+                              lineterminator='\r\n', quoting=csv.QUOTE_ALL, escapechar='\\')
+        # Add the headers and data to the CSV file.
+        csv_file.writerow(headers)
+        csv_file.writerows(results)
+        self.logger.log(log_file, "File exported successfully!!!")
+        log_file.close()
