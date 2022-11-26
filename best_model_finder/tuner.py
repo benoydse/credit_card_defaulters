dc4ab610989de8238307cbb4e054548b6a315a67
Benoy Eremita <benoy@segmenty.com>
2022-11-26 12:31:36 +0530

    change model from xgb to rfc

--- best_model_finder/tuner.py
+++ best_model_finder/tuner.py
@@ -1,6 +1,6 @@
 from sklearn.naive_bayes import GaussianNB
 from sklearn.model_selection import GridSearchCV
-from xgboost import XGBClassifier
+from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import roc_auc_score, accuracy_score
 
 
@@ -16,7 +16,7 @@ class ModelFinder:
         self.file_object = file_object
         self.logger_object = logger_object
         self.gnb = GaussianNB()
-        self.xgb = XGBClassifier(objective='binary:logistic', n_jobs=-1)
+        self.rfc = RandomForestClassifier(random_state=41)
 
     def get_best_params_for_naive_bayes(self, train_x, train_y):
         """
@@ -47,36 +47,38 @@ class ModelFinder:
                                '. Exited the get_best_params_for_naive_bayes method of the Model_Finder class')
         return self.gnb
 
-    def get_best_params_for_xgboost(self, train_x, train_y):
+    def get_best_params_for_rf(self, train_x, train_y):
         """
-        Gets the parameters for the xgboost Algorithm which gives the best accuracy. Hyperparameter Tuning is used.
+        Gets the parameters for the random forest classifier Algorithm which gives the best accuracy. Hyperparameter
+        tuning is used.
         params: train_x, train_y
-        First we initialize param_grid_xgboost which is a dictionary of parameters names as keys and lists of parameter
+        First we initialize param_grid_rfc which is a dictionary of parameters names as keys and lists of parameter
         settings to try as values --> next we create an object of the GridSearchCv class to find the best parameters -->
         next use fit on the training dataset to get the best parameters for the xgb model --> next extract the best
         parameters into 'self.var_smoothing' --> create a new model using the gnb algorithm with the best parameters.
-        returns: self.xgb --> xgboost model with the best parameters
+        returns: self.rfc --> rfc model with the best parameters
         """
         self.logger_object.log(self.file_object,
-                               'Entered the get_best_params_for_xgboost method of the Model_Finder class')
+                               'Entered the get_best_params_for_rfc method of the Model_Finder class')
         # initializing with different combination of parameters
-        param_grid_xgboost = {"n_estimators": [50, 100, 130], "max_depth": range(3, 11, 1),
-                              "random_state": [0, 50, 100]}
+        param_grid_rfc = {'n_estimators': [200, 400, 500], 'max_features': ['auto', 'sqrt', 'log2'],
+                          'max_depth': [3, 4, 6, 7], 'criterion': ['gini', 'entropy']}
         # Creating an object of the Grid Search class
-        grid = GridSearchCV(XGBClassifier(objective='binary:logistic'), param_grid_xgboost, verbose=3, cv=2, n_jobs=-1)
-        # finding the best parameters
-        grid.fit(train_x, train_y)
+        cv_rfc = GridSearchCV(estimator=self.rfc, param_grid=param_grid_rfc, cv=5)
+        cv_rfc.fit(train_x, train_y)
         # extracting the best parameters
-        random_state = grid.best_params_['random_state']
-        max_depth = grid.best_params_['max_depth']
-        n_estimators = grid.best_params_['n_estimators']
+        n_estimators = cv_rfc.best_params_['n_estimators']
+        max_features = cv_rfc.best_params_['max_features']
+        max_depth = cv_rfc.best_params_['max_depth']
+        criterion = cv_rfc.best_params_['criterion']
         # creating a new model with the best parameters
-        self.xgb = XGBClassifier(random_state=random_state, max_depth=max_depth, n_estimators=n_estimators, n_jobs=-1)
+        self.rfc = RandomForestClassifier(n_estimators=n_estimators, criterion=criterion, max_depth=max_depth,
+                                          max_features=max_features, random_state=42)
         # training the mew model
-        self.xgb.fit(train_x, train_y)
-        self.logger_object.log(self.file_object, 'XGBoost best params: ' + str(grid.best_params_) +
-                               '. Exited the get_best_params_for_xgboost method of the Model_Finder class')
-        return self.xgb
+        self.rfc.fit(train_x, train_y)
+        self.logger_object.log(self.file_object, 'rfc best params: ' + str(cv_rfc.best_params_) +
+                               '. Exited the get_best_params_for_rfc method of the Model_Finder class')
+        return self.rfc
 
     def get_best_model(self, train_x, train_y, test_x, test_y):
         """
@@ -88,17 +90,17 @@ class ModelFinder:
         returns: 'model_name', model object
         """
         self.logger_object.log(self.file_object, 'Entered the get_best_model method of the Model_Finder class')
-        # create best model for XGBoost
-        xgboost = self.get_best_params_for_xgboost(train_x, train_y)
-        prediction_xgboost = xgboost.predict(test_x)  # Predictions using the XGBoost Model
+        # create best model for rfc
+        rfc = self.get_best_params_for_rf(train_x, train_y)
+        prediction_rfc = rfc.predict(test_x)  # Predictions using the rfc Model
         # if there is only one label in y, then roc_auc_score returns error. We will use accuracy in that case
         if len(test_y.unique()) == 1:
-            xgboost_score = accuracy_score(test_y, prediction_xgboost)
-            self.logger_object.log(self.file_object, 'Accuracy for XGBoost:' + str(xgboost_score))
+            rfc_score = accuracy_score(test_y, prediction_rfc)
+            self.logger_object.log(self.file_object, 'Accuracy for XGBoost:' + str(rfc_score))
         else:
-            xgboost_score = roc_auc_score(test_y, prediction_xgboost)
-            self.logger_object.log(self.file_object, 'AUC for XGBoost:' + str(xgboost_score))
-            # create best model for naive bayes
+            rfc_score = roc_auc_score(test_y, prediction_rfc)
+            self.logger_object.log(self.file_object, 'AUC for rfc:' + str(rfc_score))
+        # create best model for naive bayes
         naive_bayes = self.get_best_params_for_naive_bayes(train_x, train_y)
         prediction_naive_bayes = naive_bayes.predict(test_x)
         if len(test_y.unique()) == 1:
@@ -106,9 +108,9 @@ class ModelFinder:
             self.logger_object.log(self.file_object, 'Accuracy for NB:' + str(naive_bayes_score))
         else:
             naive_bayes_score = roc_auc_score(test_y, prediction_naive_bayes)
-            self.logger_object.log(self.file_object, 'AUC for RF:' + str(naive_bayes_score))
+            self.logger_object.log(self.file_object, 'AUC for NB:' + str(naive_bayes_score))
         # comparing the two models
-        if naive_bayes_score < xgboost_score:
-            return 'XGBoost', xgboost
+        if naive_bayes_score < rfc_score:
+            return 'RFC', rfc
         else:
             return 'NaiveBayes', naive_bayes
